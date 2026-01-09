9cfd697a4210e23ff8a428dde652a188795a7d9b
Marcelo Gnisci <maka77@gmail.com>
2026-01-08 22:16:13 -0500

    Add story: Cuando los silencios también hablan

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 miradas: resolve(__dirname, 'miradas-complices.html'),
                 roma: resolve(__dirname, 'cuando-roma-me-llevo-al-mar.html'),
+                silencios: resolve(__dirname, 'cuando-los-silencios-tambien-hablan.html'),
             },
         },
     },
